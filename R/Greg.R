5209d9348eb018e60e35513fff3b88b7a0cdfd35
Max Gordon <max@gforge.se>
2014-08-08 13:53:50 +0200

    Update package description with more details and better matching the contents

--- R/Greg.R
+++ R/Greg.R
@@ -1,10 +1,28 @@
-#' A package for regression handling 
+#' A package that helps with regressions
 #' 
-#' This R-package provides additional functions that help you
-#'  convey regression models. It has also a few functions for
-#'  handling robust confidence intervals for the ols() regression 
-#'  in the rms-package. This package is closely related to the Gmisc-package.
+#' This R-package provides functions that primarily aimed at helping 
+#' you work with regression models. While mych of the data presented by the
+#' standard regression output is useful and important - there is often a
+#' need for further simplifaction prior to publication. The methods implemented
+#' in this package are inspired by some of the top journals such as NEJM, BMJ,
+#' and other medical journals as this is my research field. 
 #' 
-#' @name Greg
+#' @section Output functions:
+#' 
+#' The package has function that automatically prints the crude unadjusted estimates
+#' of a function next to the adjusted estimates, a common practice for medical
+#' publications. 
+#' 
+#' The forestplot wrappers allows for easily displaying regression
+#' estimates, often convenient for models with a large number of variables. 
+#' 
+#' @section Other regression functions:
+#' 
+#' In addition to these funciton the package has some extentions to linear regression
+#' where it extends the functionality by allowing for robust covariance matrices. 
+#' by integrating the sandwich-package for rms::ols().
+#' 
+#' @author Max Gordon
+#' @name Greg-package
 #' @docType package
 NULL
