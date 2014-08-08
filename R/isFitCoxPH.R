427d73edb5ad0eea451f7590231bded9c749e475
Max Gordon <max@gforge.se>
2014-08-08 14:03:11 +0200

    Minor changes and doc-merge for isFit-functions

--- R/isFitCoxPH.R
+++ R/isFitCoxPH.R
@@ -1,17 +1,18 @@
-#' Tests if function is of a cox regression type.
+#' Functions for checking regression type
 #' 
-#' Counts competing risk regression as a coxph
+#' The \emph{isFitCoxPH} A simple check if object inherits either 
+#' "coxph" or "crr" class indicating
+#' that it is a survival function.
 #' 
 #' @param fit Regression object
-#' @return boolean
+#' @return \code{boolean} Returns \code{TRUE} if the object is of that type
+#'  otherwise it returns \code{FALSE}.
 #' 
+#' @rdname isFitFn
 #' @export 
 #' @example inst/examples/isFitCoxPH_example.R
 isFitCoxPH <- function(fit){
-  if ("coxph" %in% class(fit))
-    return (TRUE)
-  
-  if ("crr" %in% class(fit))
+  if (any(c("coxph", "crr") %in% class(fit)))
     return (TRUE)
   
   return (FALSE)
--- R/isFitLogit.R
+++ R/isFitLogit.R
@@ -1,12 +1,12 @@
-#' Tests if function is of a logistic regression type
-#' 
-#' @param fit Regression object 
-#' @return boolean 
+#' The \emph{isFitLogit} is a simple check if object inherits either "lrm" 
+#' class or inherits the  "glm" class together with a "logit" link 
+#' funciton indicating that it is a logistic function.
+#'
+#' @rdname isFitFn
 #' 
 #' @example inst/examples/isFitLogit_example.R
 #' 
 #' @export
-#' @author max
 isFitLogit <- function(fit){
   if ("lrm" %in% class(fit))
     return (TRUE)
