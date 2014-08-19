d40cfe7866f0973e40dabd6c70a4106904d1e07b
Max Gordon <max@gforge.se>
2014-08-19 20:43:10 +0200

    Switched to devtools implementation of testthat

--- tests/test-all.R
+++
@@ -1,9 +0,0 @@
-library('testthat')
-library('Greg')
-
-test_check('Greg')
-## To run the tests manually
-if (FALSE){
-  my_test_dir <- gsub("Greg.*", "Greg/tests/testthat", getwd())
-  test_dir(my_test_dir)
-}
