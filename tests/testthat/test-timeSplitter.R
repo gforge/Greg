cef9240ec20a159b8e7955ffe79ffcac3623a154
Max Gordon <max@gforge.se>
2015-12-27 22:47:43 +0100

    Moved the interval test to a separate file as this generated errors using the testthat environment

--- tests/testthat/test-timeSplitter.R
+++ tests/testthat/test-timeSplitter.R
@@ -107,39 +107,4 @@ test_that("Check that age and calendar time is updated",{
   }
 })
 
-test_that("Test model interval equivalence",{
-  data("melanoma", package = "boot")
-  melanoma$status %<>% 
-    factor(
-      levels=1:3,
-      labels=c("melanoma-specific death", "alive", "other death"))
-  melanoma$ulcer %<>% 
-    factor(
-      levels=0:1,
-      labels=c("Absent", "Present")
-    )
-  melanoma$time <-
-    melanoma$time/365.25
-  library(survival)
-  regular_model <- coxph(Surv(time, status == "melanoma-specific death") ~
-                           age + sex + year + thickness + ulcer,
-                         data = melanoma,
-                         x = TRUE, y = TRUE)
-  library(magrittr)
-  spl_melanoma <-
-    melanoma %>% 
-    timeSplitter(by = .1,
-                 event_var = "status",
-                 event_start_status = "alive",
-                 time_var = "time",
-                 time_related_vars = c("age", "year"))
-  
-  interval_model <-
-    update(regular_model, 
-           Surv(Start_time, Stop_time, status == "melanoma-specific death") ~ .,
-           data = spl_melanoma)
-  
-  expect_true(abs(sum(coef(interval_model) - coef(regular_model))) < .Machine$double.eps*100)  
-})
-
   
